a4fd0de382a1cdd3ae097a05bf3df38561a0b84a
seinnlet <sllhninn.@gmail.com>
2023-09-05 21:32:53 +0900

    modified css and js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 // JavaScript Document
 
 // to close mobile menu when menu item is pressed
+
 let nav_item = document.getElementsByClassName("nav-item");
 let i = nav_item.length;
 
@@ -13,35 +14,33 @@ function removeChecked() {
 
 
 // to add navigation active, inspired from https://codepen.io/dbilanoski/pen/LabpzG
-// Get all sections that have an ID defined
+
+// get all sections that have an ID defined
 const sections = document.querySelectorAll("section[id]");
 
-// Add an event listener listening for scroll
-window.addEventListener("scroll", navHighlighter);
+window.addEventListener("scroll", addNavActive);
 
-function navHighlighter() {
+function addNavActive() {
   
-  // Get current scroll position
+  // get current y scroll position
   let scrollY = window.pageYOffset;
   
-  // Now we loop through sections to get height, top and ID values for each
+  // get height, top and ID values for each section
   sections.forEach(current => {
     const sectionHeight = current.offsetHeight;
     const sectionTop = current.offsetTop - 50;
     sectionId = current.getAttribute("id");
-    
+  // check position to add or remove active
   if(sectionId != "banner"){
     if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
       document.querySelector("nav a[href*=" + sectionId + "]").classList.add("active");
     } else {
       document.querySelector("nav a[href*=" + sectionId + "]").classList.remove("active");
     }
-   }
-      
+   } 
   });
 
 }
-
 
 
 // animation
